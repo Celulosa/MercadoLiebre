f5c1eed501273b5c38a91a968a2ed0f6f0934838
celulosa <amartinmesel@hotmail.com>
2022-09-26 18:58:16 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Servidor corriendo");
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
+
+app.get('/register', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/registro.html'));
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
